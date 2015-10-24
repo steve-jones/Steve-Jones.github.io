86b235668ff8c0ddc983b458f17ef5af5a5ca7d5
Steve-Jones <steve@stevenjon.es>
2015-10-24 18:44:14 -0400

    Added routes partials for home, about & portfolio

--- website.js
+++ website.js
@@ -1,1 +1,14 @@
-angular.module('website', []);
+
+// define module
+// define routes
+angular.module('website', []).config(function($routeProvider){ //inject routeProvider
+  $routeProvider.
+  when('/about', {template:'partials/about.html'}).
+  when('/portfolio', {template:'partials/portfolio.html'}).
+  otherwise({redirectTo:'/home', template:'partials/home.html'});
+
+});
+
+function MainCtrl($scope){
+
+}
