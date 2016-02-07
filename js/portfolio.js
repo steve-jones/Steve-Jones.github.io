ef26c4f57d622cd128eced5d4b278ee0cfd7b0c6
Steve-Jones <steve@stevenjon.es>
2016-02-06 23:47:40 -0500

    Fixed js ease / portfolio issue

---
+++ js/portfolio.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+  
+    //ISOTOPE  PLUGIN SCRIPT FOR FILTER FUCNTIONALITY
+    $(window).load(function () {
+        var $container = $('#portfolio-div');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.categories a').click(function () {
+            $('.categories .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+});
