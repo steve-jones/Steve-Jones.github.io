0a961363f2241b5a5548332f6d1db392b5bcea2e
Steve-Jones <steve@stevenjon.es>
2016-02-06 22:04:09 -0500

    Web redesign

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+
+
+    //FANCY BOX PLUGIN SCRIPT FOR POPUP IMAGE
+    $('.fancybox-media').fancybox({
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        helpers: {
+            title: {
+                type: 'inside'
+            }
+        }
+    });
+    //ISOTOPE  PLUGIN SCRIPT FOR FILTER FUCNTIONALITY
+    $(window).load(function () {
+        var $container = $('#portfolio-div');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.categories a').click(function () {
+            $('.categories .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+});
